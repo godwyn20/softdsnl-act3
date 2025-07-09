9362a63f5aa074787a6b2661807141a878ea467c
godwyn20 <gtatlonghari@student.hau.edu.ph>
2025-07-09 09:01:42 +0800

    Fix errors can't run to Postman

--- ml-custom-dataset/ml_api_project/ml_api/views.py
+++ ml-custom-dataset/ml_api_project/ml_api/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+# ml_api/views.py
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import joblib
+
+import os
+from django.conf import settings
+
+model_path = os.path.join(settings.BASE_DIR, 'ml_api', 'model.pkl')
+encoder_path = os.path.join(settings.BASE_DIR, 'ml_api', 'label_encoder.pkl')
+
+model = joblib.load(model_path)
+label_encoder = joblib.load(encoder_path)
+
+
+class PredictView(APIView):
+    def post(self, request):
+        try:
+            petal_length = float(request.data.get("petal_length"))
+            petal_width = float(request.data.get("petal_width"))
+
+            prediction = model.predict([[petal_length, petal_width]])
+            label = label_encoder.inverse_transform(prediction)[0]
+
+            return Response({"prediction": label})
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
